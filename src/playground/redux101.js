e730f3f96cbf51ed8cb588da34bae5136314db24
tsudodog <malamute29@gmail.com>
2018-10-15 19:53:52 -0500

    learning about redux

---
+++ src/playground/redux101.js
@@ -0,0 +1,58 @@
+import { createStore } from 'redux';
+
+
+const store = createStore((state = { count: 0 }, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+            return {
+                count: state.count + incrementBy
+            };
+        case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number'? action.decrementBy : 1;
+            return {
+                count: state.count - decrementBy
+            };
+        case 'RESET' :
+            return {
+                count : 0
+            };
+        default:
+            return state;
+    }
+
+
+});
+
+console.log(store.getState());
+
+// Action - than an object that gets sent to the store
+
+const unsubscribe = store.subscribe ( () => {
+    console.log(store.getState());
+})
+//increment
+store.dispatch({
+    type: 'INCREMENT',
+    'incrementBy': 5
+});
+
+//Id like to reset the count to 0
+
+store.dispatch({
+    type: 'DECREMENT',
+    'decrementBy': 100
+});
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+
+
+
+store.dispatch({
+    type: 'RESET'
+});
+
+
+console.log(store.getState());
