96e8efcb38b0b16bc399dd991063cfa4d6903c69
tsudodog <malamute29@gmail.com>
2018-10-22 20:06:10 -0500

    learned about array destructuring

--- src/playground/destructureObject.js
+++ src/playground/destructureObject.js
@@ -8,3 +8,15 @@ const book = {
 
 const {name : publisherName =''} = book.publisher;
 console.log(publisherName)
+
+
+// array example 
+const [street, city, state, zip]= address;
+console.log(`You are in ${city} ${state}`)
+
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75' ];
+
+const [itemName,,medPrice,] = item;
+
+console.log(`A medium ${itemName} costs ${medPrice}`);
