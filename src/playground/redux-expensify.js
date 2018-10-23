34457350e798ad764b7b24c5634c1a494b1dde50
tsudodog <malamute29@gmail.com>
2018-10-23 18:10:17 -0500

    added class spreading to babel

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,7 +1,32 @@
 import {createStore, combineReducers} from 'redux'
+import uuid from 'uuid'
 
 //ADD_EXPENSE
+const addExpense = (
+    {
+        description = '' , 
+        note ='', 
+        amount = 0, 
+        createdAt = 0
+    }={}
+) => ({type: 'ADD_EXPENSE',
+expense:{
+    id: uuid(), 
+    description,
+    note,
+    amount,
+    createdAt
+}})
 //REMOVE_EXPENSE
+const removeExpense = (
+    {
+        id
+    } = {}
+)=> ({
+    type: 'REMOVE_EXPENSE',
+    id
+})
+
 //EDIT_EXPENSE
 //SET_TEXT_FILTER
 //SORT_BY_DATE
@@ -15,6 +40,12 @@ const expensesReducerDefaultState = []
 
 const expensesReducer = (state = expensesReducerDefaultState, action) =>{
     switch(action.type){
+        case 'ADD_EXPENSE':
+            return [...state, action.expense]
+        case 'REMOVE_EXPENSE':
+            return state.filter(({id})=>{
+                return id !== action.id
+            })    
         default:
             return state
     }
@@ -42,6 +73,14 @@ const store = createStore(combineReducers({
 
 console.log(store.getState())
 
+store.subscribe(()=>{
+    console.log(store.getState())
+})
+
+const expense1 = store.dispatch(addExpense({description:'Rent', amount: 100}))
+const expense2 = store.dispatch(addExpense({description: 'Coffee', amount: 300}))
+
+store.dispatch(removeExpense({id: expense1.expense.id}))
 
 const demoState = {
     expenses : [{
@@ -58,3 +97,13 @@ const demoState = {
         endDate: undefined
     }
 }
+
+
+const user = {
+    name: 'Jen',
+    age: 24
+}
+
+console.log({
+    ...user
+})
