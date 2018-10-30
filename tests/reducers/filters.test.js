ff2de2859071ad52a21c53988c4a1898e3bb7fe4
tsudodog <malamute29@gmail.com>
2018-10-29 19:21:21 -0500

    unit testing is awesome!

---
+++ tests/reducers/filters.test.js
@@ -0,0 +1,66 @@
+import moment from 'moment'
+import filtersReducer from '../../src/reducers/filters'
+
+test('should setup default filter values', () => {
+    const state = filtersReducer(undefined, {type: '@@INIT'})
+    expect(state).toEqual({
+        text: '',
+        sortBy: 'date',
+        startDate: moment().startOf('month'),
+        endDate: moment().endOf('month')
+
+    })
+})
+
+test('should set sortby to amount', () =>{
+    const state = filtersReducer(undefined, {type: 'SORT_BY_AMOUNT'})
+    expect(state.sortBy).toBe('amount')
+})
+
+test('should set sortby to date', () =>{
+    const currentState = {
+        text: '',
+        startDate: undefined,
+        endDate : undefined,
+        sortBy: 'amount'
+    }
+    const action = {
+        type: 'SORT_BY_DATE'
+    }
+
+    const state = filtersReducer(currentState, action)
+    expect(state.sortBy).toBe('date')
+})
+
+
+test('should set text filter', ()=>{
+    const action ={
+        type: 'SET_TEXT_FILTER',
+        textValue:'blarg'
+    }
+    const state = filtersReducer(undefined, action)
+
+    expect(state.text).toBe('blarg')
+})
+
+
+test('should set start date filter', ()=>{
+    const action ={
+        type: 'SET_START_DATE',
+        startDate: moment(0)
+    }
+    const state = filtersReducer(undefined, action)
+
+    expect(state.startDate).toEqual(moment(0))
+})
+
+
+test('should set end date filter', ()=>{
+    const action ={
+        type: 'SET_END_DATE',
+        endDate: moment(0)
+    }
+    const state = filtersReducer(undefined, action)
+
+    expect(state.endDate).toEqual(moment(0))
+})
